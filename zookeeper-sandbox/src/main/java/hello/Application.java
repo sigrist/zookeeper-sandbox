578cd74730e14a199086679376aa53ab092faec9
Paulo Sigrist <Paulo.Sigirst@venturus.org.br>
2015-12-16 14:45:05 -0200

    Testing @EnableZuulProxy

--- zookeeper-sandbox/src/main/java/hello/Application.java
+++ zookeeper-sandbox/src/main/java/hello/Application.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableZuulProxy
 public class Application {
 
 	@Autowired
